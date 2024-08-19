f77428266da670773dfaeff6f4440779b25e5f97
kamm3r <marcokammer59@gmail.com>
2024-08-19 15:23:50 +0300

    three integer

--- ex1.py
+++ ex1.py
@@ -13,3 +13,14 @@ perimeter = (length + width) * 2
 
 print(f'rectangel perimeter is {perimeter}')
 
+three_integer = input('please enter three number ')
+a, b, c = three_integer.split()
+
+sum = int(a) + int(b) + int(c)
+product =  int(a) * int(b) * int(c)
+average = int(a) / int(b) / int(c)
+
+print(f'sum of the three integer {sum}')
+print(f'product of the three integer {product}')
+print(f'average of the three integer {average}')
+
