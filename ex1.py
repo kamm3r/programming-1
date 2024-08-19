89c22f90f743890e4f8d19b065815298ee61a6e6
kamm3r <marcokammer59@gmail.com>
2024-08-19 15:05:23 +0300

    1-3 exercise are done

---
+++ ex1.py
@@ -0,0 +1,15 @@
+name = input('enter your name: ')
+print(f'hello, {name}!')
+
+import math
+radius = float(input('what is the radius of circle: '))
+area = math.pi * radius ** 2
+print(f'area of the circle is {area}')
+
+
+length = float(input('what is the length of the rectangel: '))
+width = float(input('what is the width of the rectangel: '))
+perimeter = (length + width) * 2
+
+print(f'rectangel perimeter is {perimeter}')
+
