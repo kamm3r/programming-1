f01367d6615eac3a2acab94715efec855dd938b1
kamm3r <marcokammer59@gmail.com>
2024-08-20 12:56:41 +0300

    leap year cheker done

--- ex2.py
+++ ex2.py
@@ -48,3 +48,17 @@ elif gender == 'man' or gender == 'male':
         print('normal hemoglobin levels')
 else:
     print('not valid')
+
+
+# Write a program that asks the user to enter a year and
+# notifies the user whether the input year is a leap year.
+# A year is a leap year if it is divisible by four.
+# However, years divisible by 100 are leap years only
+# if they are also divisible by 400.
+
+year = int(input('enter a year: '))
+
+if ((year % 400 == 0) or (year % 100 != 0) and (year % 4 ==0)):
+    print('this is a leap year')
+else:
+    print('not a leap year')
