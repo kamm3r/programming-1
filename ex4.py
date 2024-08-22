2ff3623d7c8a312dc26f9e065fc32fb9798dc1ac
kamm3r <marcokammer59@gmail.com>
2024-08-22 11:13:57 +0300

    city array done

--- ex4.py
+++ ex4.py
@@ -63,3 +63,22 @@ elif num > 1:
         print(num, "is not a prime number")
     else:
         print(num, "is a prime number")
+
+# Write a program that asks the user to enter the names of five
+# cities one by one (use a for loop for reading the names)
+# and stores them into a list structure. Finally, the program prints out the
+# names of the cities one by one, one city per line, in the same
+# order they were read as input.
+# Use a for loop for asking the names and
+# a for/in loop to iterate through the list.
+# city = input('city name: ')
+
+cityArray = []
+
+for count in range(5):
+    city = input('city name: ')
+    cityArray.insert(count, city)
+
+print('cities listed')
+for city in cityArray:
+    print(f'{city}')
