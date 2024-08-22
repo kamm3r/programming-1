1730acc2a0874ff2c07f73e96553cf8b15051d87
kamm3r <marcokammer59@gmail.com>
2024-08-22 10:57:11 +0300

    prime number checker done

--- ex4.py
+++ ex4.py
@@ -40,3 +40,26 @@ for i in array:
 max(array)
 array.sort(reverse=True)
 print(f'array {array[0:5]}')
+
+# Write a program that asks the user for an integer and tells
+# if the number is a prime number.
+# Prime numbers are number that are only divisible by one or the number itself.
+#   For example, 13 is a prime number as it can only be divided by 1 or 13 so that the result is an integer.
+#   On the other hand, 21 is not a prime number as it is divisible by 3 and 7.
+
+num = int(input("Enter a number: "))
+
+flag = False
+
+if num == 0 or num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
