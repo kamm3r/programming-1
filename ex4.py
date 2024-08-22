e3e0a3b55f72f98a5e72b2f28bf68d1bd6ab5020
kamm3r <marcokammer59@gmail.com>
2024-08-22 10:52:11 +0300

    forloop array descending order done

--- ex4.py
+++ ex4.py
@@ -13,3 +13,30 @@ for dice in range(dices):
     sum += roll
 
 print(f'rolled dice {result} sum is {sum}')
+
+# Write a program that asks the user to enter numbers until
+# they input an empty string to quit.
+# At the end, the program prints out the five greatest
+# numbers sorted in descending order.
+# Hint: You can reverse the order of sorted list items
+# by using the sort method with the reverse=True argument.
+
+ask = input('enter number: ')
+
+array = []
+if ask != '':
+    ask = int(ask)
+    array.insert(0, ask)
+count = 0
+
+for i in array:
+    value = input('enter number: ')
+    if value == '':
+        break
+    value = int(value)
+    count += 1
+    array.insert(count, value)
+
+max(array)
+array.sort(reverse=True)
+print(f'array {array[0:5]}')
