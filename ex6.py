da22aa3923f3b8009b61f0e6f1e3680121830536
kamm3r <marcokammer59@gmail.com>
2024-08-26 13:47:12 +0300

    name tuple in progress

--- ex6.py
+++ ex6.py
@@ -22,3 +22,14 @@ elif monthNumber == 12 or monthNumber == 1 or monthNumber == 2:
     print(f'{season[3]}')
 else:
     print('invalid number')
+
+# Write a program that asks the user to enter names until
+# he/she enters an empty string. After each name is read the program
+# either prints out New name or Existing name depending on whether
+# the name was entered for the first time. Finally, the program lists out
+# the input names one by one, one below another in any order.
+# Use the set data structure to store the names.
+
+nameTuple = "new name", "existing name"
+
+
