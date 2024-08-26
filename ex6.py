612069339878b2873dd33d135e8fa9a43d5da81b
kamm3r <marcokammer59@gmail.com>
2024-08-26 13:38:04 +0300

    season tuple done

---
+++ ex6.py
@@ -0,0 +1,24 @@
+# Write a program that asks the user for a number of a month
+# and then prints out the corresponding season (spring, summer, autumn, winter)
+# Save the seasons as strings into a tuple in your program. We can define
+# each season to last three months, December being the first month of winter.
+
+monthNumber =int(input('enter month number: '))
+
+season = "spring", "summer", "autumn", "winter"
+
+spring = 3, 4, 5
+summer = 6, 7, 8
+autumn = 9, 10, 11
+winter = 12, 1, 2
+
+if monthNumber == 3 or monthNumber == 4 or monthNumber == 5:
+    print(f'{season[0]}')
+elif monthNumber == 6 or monthNumber == 7 or monthNumber == 8:
+    print(f'{season[1]}')
+elif monthNumber == 9 or monthNumber == 10 or monthNumber == 11:
+    print(f'{season[2]}')
+elif monthNumber == 12 or monthNumber == 1 or monthNumber == 2:
+    print(f'{season[3]}')
+else:
+    print('invalid number')
