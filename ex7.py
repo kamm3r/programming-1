1b8a494c90c66a9287651833e4c7509e6d740384
kamm3r <marcokammer59@gmail.com>
2024-08-27 11:12:43 +0300

    not done yet skipping it for now until i do database course

---
+++ ex7.py
@@ -0,0 +1,5 @@
+# Write a program that asks the user to enter the ICAO code of an airport.
+# The program fetches and prints out the corresponding airport
+# name and location (town) from the airport database used on this course.
+# The ICAO codes are stored in the ident column of the airport table.
+
