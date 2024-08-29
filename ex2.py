ea518aeab24f4b6e24c3dfcd5d6df2bfdbb0fe62
kamm3r <marcokammer59@gmail.com>
2024-08-29 13:43:49 +0300

    added ex2 problem descriptions above of the solutions

--- ex2.py
+++ ex2.py
@@ -1,4 +1,8 @@
-# zander(kuha) minimum size is 42 cm
+# Write a program that asks a fisher the length of a zander in centimeters.
+# If the zander does not fulfill the size limit, the program instructs
+# to release the fish back into the lake and notifies the user of
+# how many centimeters below the size limit the caught fish was.
+# A zander must be 42 centimeters or longer to meet the size limit.
 
 size = float(input('zander size is: '))
 
@@ -7,7 +11,16 @@ if size >= 42:
 else:
     print('this does not meet the required size L')
 
-# poor vs rich brother
+# Write a program that asks the user to enter the cabin class of
+# a cruise ship and then prints out a written description
+# according to the list below. You must use
+# an if/elif/else structure in your solution.
+  # LUX: upper-deck cabin with a balcony.
+  # A: above the car deck, equipped with a window.
+  # B: windowless cabin above the car deck.
+  # C: windowless cabin below the car deck.
+# If the user enters an invalid cabin class, the program outputs
+# an error message Invalid cabin class.
 
 print('cabin classes LUX, A, B, C')
 cabin = input('choose the richness status: ')
--- ex8.py
+++ ex8.py
@@ -6,13 +6,13 @@
 # Write a main program where you create a new car
 # (registration number ABC-123, maximum speed 142 km/h).
 # Finally, print out all the properties of the new car.
-
+import random
 
 class Car:
     def __init__(self, registrationNumber, maxSpeed):
         self.registrationNumber = registrationNumber
         self.maxSpeed = maxSpeed
-        self.currentSpeed = 0
+        self.speed = 0
         self.distance = 0
 
     # Extend the program by adding an accelerate method into the new class.
@@ -28,24 +28,67 @@ class Car:
     # The travelled distance does not have to be updated yet.
 
     def accelerate(self, changeOfSpeed):
-        if changeOfSpeed > self.maxSpeed:
-            print('cannot be more than car max speed')
-        elif self.currentSpeed > 0 or self.currentSpeed == 0:
-            self.currentSpeed += changeOfSpeed
-            print(f'current speed is {self.currentSpeed}')
-        else:
-            print('car has stopped moving')
+        changedSpeed = self.speed + changeOfSpeed
+        self.speed = max(0, min(changedSpeed, self.maxSpeed))
+
+    # Again, extend the program by adding a new drive method that receives
+    # the number of hours as a parameter. The method increases the travelled
+    # distance by how much the car has travelled in constant speed in
+    # the given time. Example: The travelled distance of car
+    # object is 2000 km. The current speed is 60 km/h.
+    # Method call car.drive(1.5) increases the travelled distance to 2090 km.
+
+    def drive(self, hours):
+        self.distance = hours * self.speed
 
 def main():
     newCar = Car('ABC-123', 142)
-    newCar.accelerate(30)
-    newCar.accelerate(70)
-    newCar.accelerate(50)
     print('all the properties of the new car')
     print(f'registeration number {newCar.registrationNumber}')
     print(f'max speed {newCar.maxSpeed} km/h')
-    print(f'current speed {newCar.currentSpeed} km/h')
+    print(f'current speed {newCar.speed} km/h')
     print(f'distance travelled {newCar.distance}')
+
+    newCar.accelerate(30)
+    newCar.accelerate(70)
+    newCar.accelerate(50)
+    print(f'current speed after acceleration: {newCar.speed} km/h')
+
+    newCar.accelerate(-200)
+    print(f'emergency brake: {newCar.speed} km/h')
+
+    # Now we will program a car race. The travelled distance of a new car
+    # is initialized as zero. At the beginning of the main program, create
+    # a list that consists of 10 car objects created using a loop. The
+    # maximum speed of each new car is a random value between 100 km/h
+    # and 200 km/h. The registration numbers are created as
+    # follows: "ABC-1", "ABC-2" and so on. Now the race begins. One per
+    # every hour of the race, the following operations are performed:
+    # The speed of each car is changed so that the change in speed is a
+    # random value between -10 km/h and +15 km/h.
+    # This is done using the accerelate method. Each car is made to drive
+    # for one hour. This is done with the drive method.
+
+    carArray = []
+    for i in range(1,11):
+        carArray.append(Car(f'ABC-{i}',random.randint(100, 200)))
+
+    print('race started')
+    raceTime = 0
+    while True:
+        raceTime += 1
+        for car in carArray:
+            car.accelerate(random.randint(-10,15))
+            car.drive(1)
+
+        if any(car.distance >= 10000 for car in carArray):
+            break
+
+    print("\ncar race results:")
+    print("{:15s} {:10s} {:10s}".format("registration", "max speed", "distance"))
+    print("-" * 40)
+    for car in carArray:
+        print("{:15s} {:10d} {:10.2f} km".format(car.registrationNumber, car.maxSpeed, car.distance))
 
 main()
 
