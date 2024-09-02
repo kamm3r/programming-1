c651156e0bf5a430f66866b47527c5f2e34c505a
kamm3r <marcokammer59@gmail.com>
2024-09-02 13:37:33 +0300

    started creating chuck norris joke function

---
+++ ex10.py
@@ -0,0 +1,6 @@
+# Write a program that fetches and prints out a random Chuck Norris
+# joke for the user. Use the API presented here:
+# https://api.chucknorris.io/. The user should only be shown the joke text.
+
+def getCNJoke():
+
