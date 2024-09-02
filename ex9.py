3f15c4126f37f875a3666dd406d453dda7a8fe15
kamm3r <marcokammer59@gmail.com>
2024-09-02 13:34:24 +0300

    car inheritance ev and gas car subclasses done

--- ex9.py
+++ ex9.py
@@ -31,6 +31,31 @@ class magazine(publication):
     def printInfo(self):
         print(f'chief editor: {self.chiefEditor}')
 
+# Extend the previosly written Car class by adding two subclasses:
+# ElectricCar and GasolineCar. Electric cars have the capacity of
+# the battery in kilowatt-hours as their property. Gasoline cars have
+# the volume of the tank in liters as their property.
+# Write initializers for the subclasses. For example, the initializer
+# of electric cars receives the registration number, maximum speed and
+# battery capacity as its parameter. It calls the initializer of
+# the base class to set the first two properties and then sets its capacity.
+# Write a main program where you create one electric car
+# (ABC-15, 180 km/h, 52.5 kWh) and one gasoline car
+# (ACD-123, 165 km/h, 32.3 l). Select speeds for both cars, make them
+# drive for three hours and print out the values of their kilometer counters.
+
+from ex8 import Car
+
+class electricCar(Car):
+    def __init__(self, registrationNumber, maxSpeed, batteryCapacity):
+        super().__init__(registrationNumber, maxSpeed)
+        self.batteryCapacity = batteryCapacity
+
+class gasCar(Car):
+    def __init__(self, registrationNumber, maxSpeed, tankVolume):
+        super().__init__(registrationNumber, maxSpeed)
+        self.tankVolume = tankVolume
+
 def main():
     mag = magazine("Aki Hyypiä")
 
@@ -38,5 +63,16 @@ def main():
 
     mag.printInfo()
     ebook.printInfo()
+
+    evCar = electricCar('ABC-15', 180, 52.5)
+    car = gasCar('ACD-123', 165, 32.3)
+
+    evCar.speed = 40
+    car.speed = 80
+    evCar.drive(3)
+    car.drive(3)
+
+    print(f'ev drove {evCar.distance} km')
+    print(f'gas car drove {car.distance} km')
 
 main()
